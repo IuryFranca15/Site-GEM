992f681d780382a1a6ffc56c32b2d4c11f8e1352
VictorbSilva <victorbasilio5@gmail.com>
2025-05-06 14:18:40 -0300

    :arrow_up: Adicionando componente de header + logo e título da página

--- siteGEM/src/components/Navbar.jsx
+++ siteGEM/src/components/Navbar.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-import '../index.css'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo-gem.png'
+import styles from './Navbar.module.css'
+import { FaSearch } from "react-icons/fa";
 
-const navbar = () => {
+const Navbar = () => { 
+
   return (
-    <div>navbar</div>
+    <nav className={styles.menu}>
+      <div className={styles.logoContainer}>
+        <Link to='/'>
+            <img 
+                src={logo} 
+                alt="logo do site"  
+                className={styles.logoImg} 
+            />
+        </Link>
+      </div>
+
+      <div className={styles.pesquisaContainer}>
+        <input 
+          type="text" 
+          placeholder='Pesquisar' 
+          className={styles.inputPesquisa} 
+        />
+        <button className={styles.pesquisaButton}>
+          <FaSearch className={styles.pesquisaIcone} />
+        </button>
+      </div>
+
+      <div className={styles.menuLinks}>
+            <Link to='/'>Página Inicial</Link>
+            <Link to='#'>Publicações</Link>
+            <Link to='#'>Equipe</Link>
+            <Link to='#'>Subgrupos</Link>
+      </div>
+
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar;
