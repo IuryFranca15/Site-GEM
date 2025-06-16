0e30ba70139a62f023ffab925e9485ce692ecace
Pablo Rossoni <pablorossoni107@outlook.com>
2025-06-15 22:35:18 -0300

    feat: dados para a equipe

---
+++ siteGEM/src/data/teamData.jsx
@@ -0,0 +1,90 @@
+import bg from '../assets/image/pesca.jpg'
+import membroEquipe from '../assets/image/Team.webp'
+import icone from '../assets/image/pesca-icone.png'
+import iconeGem from '../assets/logoGemColored.png'
+
+const teamMembers = [
+  {
+    id: '1',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Thauan Santos',
+    grupo: 'Coordenador do GEM',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: iconeGem
+  },
+  {
+    id: '2',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Maria Silva',
+    grupo: 'Tecnologia',
+    descricao: 'Especialista em inovação agrícola',
+    iconeGrupo: icone
+  },
+  {
+    id: '3',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Gabriel Ralile',
+    grupo: 'Pesca e agricultura',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: icone
+  },
+  {
+    id: '4',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Gabriel Ralile',
+    grupo: 'Pesca e agricultura',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: icone
+  },
+  {
+    id: '5',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Maria Silva',
+    grupo: 'Tecnologia',
+    descricao: 'Especialista em inovação agrícola',
+    iconeGrupo: icone
+  },
+  {
+    id: '6',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Gabriel Ralile',
+    grupo: 'Pesca e agricultura',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: icone
+  },
+  {
+    id: '7',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Gabriel Ralile',
+    grupo: 'Pesca e agricultura',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: icone
+  },
+  {
+    id: '8',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Maria Silva',
+    grupo: 'Tecnologia',
+    descricao: 'Especialista em inovação agrícola',
+    iconeGrupo: icone
+  },
+  {
+    id: '9',
+    bg: bg,
+    foto: membroEquipe,
+    nome: 'Gabriel Ralile',
+    grupo: 'Pesca e agricultura',
+    descricao: 'Pesquisador do subgrupo Pesca e Agricultura',
+    iconeGrupo: icone
+  },
+]
+
+export default teamMembers
