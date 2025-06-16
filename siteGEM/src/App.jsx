5fd4c2d7177f3829038cdc4782ef6dcdfb231a1a
Pablo Rossoni <pablorossoni107@outlook.com>
2025-06-15 22:32:05 -0300

    feat: adiciona rota

--- siteGEM/src/App.jsx
+++ siteGEM/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/NavBar/Navbar.jsx'
 import Home from './pages/HomePage.jsx'
 import SubGroups from './pages/SubGroupsPage.jsx'
-import Team from './pages/TeamPage.jsx'
+// import Team from './pages/TeamPage.jsx'
+import TeamPage from './pages/TeamPage.jsx'
+import MemberPage from './pages/MemberPage.jsx'
 
 import Footer from './components/Footer/Footer.jsx'
 import './index.css'
@@ -14,7 +16,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/equipe' element={<Team />} />
+        <Route path='/equipe' element={<TeamPage />} />
+        <Route path="/equipe/:id" element={<MemberPage />} />
         <Route path='/subgrupos' element={<SubGroups />} />
       </Routes>
       <Footer />
