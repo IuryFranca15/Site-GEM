dfcea112b83e5e2c66e3171499a792315364c366
Pablo Rossoni <pablorossoni107@outlook.com>
2025-06-25 09:07:11 -0300

    feat: adiciona informações de exemplo dos subgrupos

---
+++ siteGEM/src/data/groupData.jsx
@@ -0,0 +1,103 @@
+import bg from '../assets/image/ex-bg-grupo.webp'
+import icone from '../assets/image/pesca-icone.png'
+
+const groupData = [
+  {
+    id: '1',
+    iconeGrupo: icone,
+    nomeGrupo: 'Blue Finance',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '2',
+    iconeGrupo: icone,
+    nomeGrupo: 'Clima e meio ambiente',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '3',
+    iconeGrupo: icone,
+    nomeGrupo: 'Conceitos e métodos',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+
+  {
+    id: '4',
+    iconeGrupo: icone,
+    nomeGrupo: 'Construção e reparação naval',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '5',
+    iconeGrupo: icone,
+    nomeGrupo: 'Defesa e segurança',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '6',
+    iconeGrupo: icone,
+    nomeGrupo: 'Energias offshore',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '7',
+    iconeGrupo: icone,
+    nomeGrupo: 'Minerais offshore',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '8',
+    iconeGrupo: icone,
+    nomeGrupo: 'Pesca e aquicultura',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '9',
+    iconeGrupo: icone,
+    nomeGrupo: 'Relações geopolíticas',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '10',
+    iconeGrupo: icone,
+    nomeGrupo: 'Relações sociais',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '11',
+    iconeGrupo: icone,
+    nomeGrupo: 'Transporte e infraestrutura',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+
+  {
+    id: '12',
+    iconeGrupo: icone,
+    nomeGrupo: 'Turismo, esporte e lazer',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    bg: bg,
+  },
+]
+
+export default groupData
