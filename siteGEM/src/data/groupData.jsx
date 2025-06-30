949c14e08c685639e9ba132ed46472d9660285f0
Pablo Rossoni <pablorossoni107@outlook.com>
2025-06-30 13:54:17 -0300

    feat: att dados

--- siteGEM/src/data/groupData.jsx
+++ siteGEM/src/data/groupData.jsx
@@ -5,24 +5,24 @@ const groupData = [
   {
     id: '1',
     iconeGrupo: icone,
-    nomeGrupo: 'Blue Finance',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Clima e meio ambiente',
+    descricao: 'Com foco em como as atividades econômicas costeiras — como pesca, turismo, transporte marítimo e energia — impactam os ecossistemas marinhos. O grupo analisa questões como poluição, mudanças climáticas, gestão de recursos naturais e recuperação de áreas degradadas, como baías e manguezais. Também propõe políticas públicas e estratégias sustentáveis que promovam o desenvolvimento da economia do mar de forma equilibrada com a conservação ambiental.',
     bg: bg,
   },
 
   {
     id: '2',
     iconeGrupo: icone,
-    nomeGrupo: 'Clima e meio ambiente',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Energias Offshore',
+    descricao: 'As energias offshore são geradas a partir de fontes naturais no ambiente marinho, como ventos (energia eólica offshore), marés e ondas. Esse tipo de energia é considerado limpo e renovável, contribuindo para a redução das emissões de gases do efeito estufa. Além disso, representa uma alternativa estratégica para diversificar a matriz energética, principalmente em regiões costeiras com alto potencial. O desenvolvimento de projetos offshore também estimula a economia do mar, gerando empregos e inovação tecnológica.',
     bg: bg,
   },
 
   {
     id: '3',
     iconeGrupo: icone,
-    nomeGrupo: 'Conceitos e métodos',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Pesca e Agricultura',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
@@ -30,48 +30,48 @@ const groupData = [
   {
     id: '4',
     iconeGrupo: icone,
-    nomeGrupo: 'Construção e reparação naval',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Conceitos e métodos',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '5',
     iconeGrupo: icone,
-    nomeGrupo: 'Defesa e segurança',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Defesa e Segurança',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '6',
     iconeGrupo: icone,
-    nomeGrupo: 'Energias offshore',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Blue Finance',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '7',
     iconeGrupo: icone,
-    nomeGrupo: 'Minerais offshore',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Transporte Marítimo',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '8',
     iconeGrupo: icone,
-    nomeGrupo: 'Pesca e aquicultura',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Geopolitica',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '9',
     iconeGrupo: icone,
-    nomeGrupo: 'Relações geopolíticas',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Transportes e Infraestrutura',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
@@ -79,15 +79,15 @@ const groupData = [
     id: '10',
     iconeGrupo: icone,
     nomeGrupo: 'Relações sociais',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
   {
     id: '11',
     iconeGrupo: icone,
-    nomeGrupo: 'Transporte e infraestrutura',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    nomeGrupo: 'Minerais offshore',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 
@@ -95,7 +95,7 @@ const groupData = [
     id: '12',
     iconeGrupo: icone,
     nomeGrupo: 'Turismo, esporte e lazer',
-    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc..........................................................................................................................',
+    descricao: 'Breve resumo do subgrupo, falando um pouco do que a equipe está responsável etc etc etc',
     bg: bg,
   },
 ]
