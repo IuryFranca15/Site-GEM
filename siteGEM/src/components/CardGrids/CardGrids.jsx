8167450fd5137cc09407c3de90ad8b3f151a2b88
Pablo Rossoni <pablorossoni107@outlook.com>
2025-08-04 17:31:14 -0300

    style: ajustes

--- siteGEM/src/components/CardGrids/CardGrids.jsx
+++ siteGEM/src/components/CardGrids/CardGrids.jsx
@@ -1,33 +1,39 @@
+import { Link } from "react-router-dom";
 import styles from "./CardsGrid.module.css";
+
+
 
 const CardsGrid = ({ data }) => {
   if (!data.length) return <p className={styles.titleNotFound}>Nenhuma publicação encontrada.</p>;
 
   return (
-    <div className={styles.grid}>
-      {data.map(({ id, title, group, type, year, image }) => (
-        <div key={id} className={styles.card}>
-          {image && (
-            <img
-              src={image}
-              alt={title}
-              className={styles.cardImage}
-            />
-          )}
-          <h4>{title}</h4>
-          <p>
-            <strong>Grupo:</strong> {group}
-          </p>
-          <p>
-            <strong>Tipo:</strong> {type}
-          </p>
-          <p>
-            <strong>Ano:</strong> {year}
-          </p>
-        </div>
-      ))}
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        {data.map(({ id, title, image, author }) => (
+          <Link>
+            <div key={id} className={styles.card}>
+              <div className={styles.imageContainer}>
+                {image && (
+                  <img
+                    src={image}
+                    alt={title}
+                    className={styles.cardImage}
+                  />
+                )}
+                <div className={styles.overlay}>
+                  <h2>{title}</h2>
+                  <h3>Por {author}</h3>
+                </div>
+              </div>
+            </div>
+          </Link>
+
+        ))}
+      </div>
     </div>
-  );
+
+
+  )
 };
 
 export default CardsGrid;
